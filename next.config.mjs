533740d363147e97e36c98a0f0753fbd03c9c392
Akansh Sirohi <akanshjaat@gmail.com>
2024-03-13 23:59:38 +0530

    Template added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+// DEBUG
+const sharex_package_name = 'debug';
+
+// PRODUCTION
+// const sharex_package_name = 'sharex.next.app';
+
+// Build - Debugging or Production
+// const basePath = `/SharexApp/${sharex_package_name}`;
+
+// Live - Debugging
+const basePath = "";
+
+const nextConfig = {
+    output: 'export',
+    basePath: basePath,
+    assetPrefix: basePath,
+    images: { 
+        unoptimized: true 
+    },
+};
 
 export default nextConfig;
